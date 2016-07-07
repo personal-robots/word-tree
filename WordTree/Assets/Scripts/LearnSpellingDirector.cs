37aba211dba15fa024c779a846606136f2f91d68
Michal Shlapentokh-Rothman <michalshlapentokh-rothman@dhcp-18-189-9-121.dyn.mit.edu>
2016-07-07 19:04:02 -0400

    Combine For Loops and Fix Spacing in Learn Spelling
    -Combine three for loops into one foreach loop
    -Fix spacing so brackets line up
    -Edit comments to reflect new changes

--- WordTree/Assets/Scripts/LearnSpellingDirector.cs
+++ WordTree/Assets/Scripts/LearnSpellingDirector.cs
@@ -8,51 +8,52 @@ using System.Linq;
 
 namespace WordTree
 {
-	public class LearnSpellingDirector : MonoBehaviour {
-		private List<Vector3> usedPos;
-		private Rect rect_but;
+	public class LearnSpellingDirector : MonoBehaviour
+	{
+		
 		// called on start, initialize stuff
-		void Start () {
+		void Start ()
+		{
 			//create instance of grestureManager
-			GestureManager gestureManager =GameObject.FindGameObjectWithTag
-				(Constants.Tags.TAG_GESTURE_MANAGER).GetComponent<GestureManager> ();
+			GestureManager gestureManager = GameObject.FindGameObjectWithTag
+				(Constants.Tags.TAG_GESTURE_MANAGER).GetComponent<GestureManager>();
 			// create two sets of words - movable and target
-			LoadSpellingLesson (ProgressManager.currentWord);
+			LoadSpellingLesson(ProgressManager.currentWord);
 
 			// subscribe buttons to gestures
-			GameObject[] buttons = GameObject.FindGameObjectsWithTag (Constants.Tags.TAG_BUTTON);
+			GameObject[] buttons = GameObject.FindGameObjectsWithTag(Constants.Tags.TAG_BUTTON);
 			foreach (GameObject button in buttons)
-				gestureManager.AddAndSubscribeToGestures (button);
+				gestureManager.AddAndSubscribeToGestures(button);
 
 			// play word's sound
-			GameObject word = GameObject.FindGameObjectWithTag (Constants.Tags.TAG_WORD_OBJECT);
-			word.GetComponent<AudioSource>().Play();
+			GameObject word = GameObject.FindGameObjectWithTag(Constants.Tags.TAG_WORD_OBJECT);
+			word.GetComponent<AudioSource> ().Play ();
 
 			// start pulsing movable letters
 			StartCoroutine (StartPulsing (.5f));
 
 			// then explode the letters
-			StartCoroutine(ExplodeWord(1));
+			StartCoroutine (ExplodeWord (1));
 
 			// then enable collisions to occur
-			StartCoroutine (EnableCollisions(2));
+			StartCoroutine (EnableCollisions (2));
 
 
 		}
 
 		// create all letters and word object
-		void LoadSpellingLesson(string word)
+		void LoadSpellingLesson (string word)
 		{
 			// get properties of current word being learned
-			WordProperties prop = WordProperties.GetWordProperties (word);
-			string[] phonemes = prop.Phonemes (); // phonemes in word
-			float objScale = prop.ObjScale (); // scale of object
+			WordProperties prop = WordProperties.GetWordProperties(word);
+			string[] phonemes = prop.Phonemes(); // phonemes in word
+			float objScale = prop.ObjScale(); // scale of object
 
 			// create movable and target letters
-			WordCreation.CreateMovableAndTargetWords (word, phonemes);
+			WordCreation.CreateMovableAndTargetWords(word, phonemes);
 
 			// create word object
-			CreateWordImage (word, objScale);
+			CreateWordImage(word, objScale);
 
 		}
 
@@ -62,144 +63,74 @@ namespace WordTree
 			float y = 2; // y-position of object
 
 			// instantiate word object from properties given
-			ObjectProperties Obj = ObjectProperties.CreateInstance (word, "WordObject", new Vector3 (0, y, 0), new Vector3 (scale, scale, 1), ProgressManager.currentLevel + "/" + word, "Words/" + word);
-			ObjectProperties.InstantiateObject (Obj);
+			ObjectProperties Obj = ObjectProperties.CreateInstance(word, "WordObject", new Vector3 (0, y, 0), new Vector3 (scale, scale, 1), ProgressManager.currentLevel + "/" + word, "Words/" + word);
+			ObjectProperties.InstantiateObject(Obj);
 		}
 
 		// explode letters of word
 		// currently handles words with 3-5 letters
 		IEnumerator ExplodeWord(float delayTime)
 		{
-			//List of possible locations for letters to go
-			List<Vector3> points= new List<Vector3>(); 
-			//List of numbers for regions 
-			List<int> points_ind= new List<int>(new int[] {1,2,3,4,5});
-			//Temporary list of numbers for use in for loop
-			List<int> points_ind_temp= new List<int>(); 
-
-
-			// wait for scene to load before exploding
-			yield return new WaitForSeconds (delayTime);
 			System.Random rnd = new System.Random();
+			//List of possible locations for letters to go
+			List<Vector3> points = new List<Vector3>();
+			//List of locations already used for letters
+			List<int> points_temp = new List<int>();
+			// wait for scene to load before exploding
+			yield return new WaitForSeconds(delayTime);
 			// find movable letters
-			GameObject[] gos = GameObject.FindGameObjectsWithTag (Constants.Tags.TAG_MOVABLE_LETTER);
-
-			Vector3[] posn = new Vector3[gos.Length]; // contains desired position to move each letter to
-			Vector3[] shuffledPosn = new Vector3[gos.Length]; // contains the new positions after being shuffled
+			GameObject[] gos = GameObject.FindGameObjectsWithTag(Constants.Tags.TAG_MOVABLE_LETTER);
 			//Possible regions where letter can move
-			points.Add( new Vector3(-10,4,0)); 
-			points.Add(new Vector3 (-10, -3,0));
-			points.Add (new Vector3 (-6, 0,0));
-			points.Add(new Vector3 (4, 3, 0));
-			points.Add(new Vector3 (8, 0, 0));
-
-							
-
-				//Random rnd = new Random();
-				if (gos.Length == 3) {
-					for (int i= 0; i<3; i++){
-					//Want to go through loop three times for three letters
-					int index = rnd.Next(0,4-i);
-					//Want random index between 0 and 4 
-					//to get a number from the list of point names
-					points_ind_temp.Add(points_ind[index]);
-					//Add point to temporary list
-					points_ind.RemoveAt(index);
-					//Remove point so it is not used again 
-				}	//Set vectors equal to an index on temporary list
-				//which contains values from 0-4 
-				//and then use that value for choosing an index from the original points list
-					posn = new Vector3[3] {
-						points[points_ind_temp[0]],
-						points[points_ind_temp[1]],
-					points[points_ind_temp[2]]
-					};
-
-				}
-	
-					
-				
-				
-				if (gos.Length == 4) {
-				for (int i = 0; i < 4; i++) {
-					int index = rnd.Next (0, 4 - i);
-					points_ind_temp.Add (points_ind [index]);
-					points_ind.RemoveAt (index);
-				}
-					posn = new Vector3[4] {
-					points[points_ind_temp[0]],
-					points[points_ind_temp[1]],
-					points[points_ind_temp[2]],
-					points[points_ind_temp[3]]
-					};
-				}
-				if (gos.Length == 5) {
-				for (int i = 0; i < 5; i++) {
-					int index = rnd.Next (0, 4 - i);
-					points_ind_temp.Add (points_ind [index]);
-					points_ind.RemoveAt (index);
-				}
-					posn = new Vector3[5] {
-						points[points_ind_temp[0]],
-						points[points_ind_temp[1]],
-						points[points_ind_temp[2]],
-						points[points_ind_temp[3]],
-						points[points_ind_temp[4]]
-					};
-			}
-
-			// shuffle the letters' positions
-			shuffledPosn = ShuffleArray (posn);
-
-			for (int i=0; i<gos.Length; i++) {
-				// move letter to desired position
-				LeanTween.move(gos[i],shuffledPosn[i],1.0f);
-
-				// rotate letter around once
-				LeanTween.rotateAround (gos[i], Vector3.forward, 360f, 1.0f);
-
-			}
-			Debug.Log ("Exploded draggable letters");
-		}
-
-		// shuffle array
-		Vector3[] ShuffleArray(Vector3[] array)
-		{
-				for (int i = array.Length; i > 0; i--)
+			points.Add (new Vector3(-10f, 4f, 0f)); 
+			points.Add (new Vector3(-10f, -3f, 0f));
+			points.Add (new Vector3(-6f, 0f, 0f));
+			points.Add (new Vector3(4f, 3f, 0f));
+			points.Add (new Vector3(8f, 0f, 0f));
+			//Change order of list of points
+			List<Vector3> points_rand = points.OrderBy(x => rnd.Next ()).ToList();
+			foreach (GameObject letter in gos)
 			{
-				int j = Random.Range (0,i);
-				Vector3 temp = array[j];
-				array[j] = array[i - 1];
-				array[i - 1]  = temp;
+				index:
+				int index = rnd.Next(0, gos.Length);
+				if (!points_temp.Contains(index)) 
+				{
+					Vector3 new_position = points_rand[index];
+					points_temp.Add(index);
+					LeanTween.move(letter, new_position, 1.0f);
+					LeanTween.rotateAround(letter, Vector3.forward, 360f, 1.0f);
+				} 
+				else 
+				{
+					goto index;
+				}
 			}
-			return array;
 		}
-
 		// enable collisions between target and movable letters
 		IEnumerator EnableCollisions(float delayTime)
 		{
 			// wait for letters to explode before enabling collisions
 			// so letters don't collide prematurely and stick together
-			yield return new WaitForSeconds (delayTime);
+			yield return new WaitForSeconds(delayTime);
 
 			// find target letters
-			GameObject[] gos = GameObject.FindGameObjectsWithTag (Constants.Tags.TAG_TARGET_LETTER); 
+			GameObject[] gos = GameObject.FindGameObjectsWithTag(Constants.Tags.TAG_TARGET_LETTER); 
 			foreach (GameObject go in gos)
 				// add collision manager so we can get trigger enter events
 				go.AddComponent<CollisionManager> ();
-			Debug.Log ("Enabled Collisions");
+			Debug.Log("Enabled Collisions");
 		}
 
 		// start pulsing draggable letters
 		IEnumerator StartPulsing(float delayTime)
 		{
 			// wait for scene to load before pulsing
-			yield return new WaitForSeconds (delayTime);
+			yield return new WaitForSeconds(delayTime);
 
 			// start pulsing letters
-			GameObject[] gos = GameObject.FindGameObjectsWithTag (Constants.Tags.TAG_MOVABLE_LETTER);
-			foreach (GameObject go in gos) {
-				go.GetComponent<PulseBehavior> ().StartPulsing (go);
+			GameObject[] gos = GameObject.FindGameObjectsWithTag(Constants.Tags.TAG_MOVABLE_LETTER);
+			foreach (GameObject go in gos)
+			{
+				go.GetComponent<PulseBehavior>().StartPulsing(go);
 			}
 		}
 
@@ -210,37 +141,38 @@ namespace WordTree
 			 
 			float time = 1f; // time to complete animation
 
-			GameObject go = GameObject.FindGameObjectWithTag (Constants.Tags.TAG_WORD_OBJECT);
+			GameObject go = GameObject.FindGameObjectWithTag(Constants.Tags.TAG_WORD_OBJECT);
 
 			Debug.Log ("Spinning " + go.name);
 
 			// spin object around once
-			LeanTween.rotateAround (go, Vector3.forward, 360f, time).setDelay(delayTime);
+			LeanTween.rotateAround(go, Vector3.forward, 360f, time).setDelay(delayTime);
 
 			// scale object up
-			LeanTween.scale (go,new Vector3(go.transform.localScale.x*1.3f,go.transform.localScale.y*1.3f,1),time).setDelay (delayTime);
+			LeanTween.scale(go, new Vector3 (go.transform.localScale.x * 1.3f, go.transform.localScale.y * 1.3f, 1), time).setDelay (delayTime);
 
 			// move object down
-			LeanTween.moveY (go, 1.5f, time).setDelay (delayTime);
+			LeanTween.moveY(go, 1.5f, time).setDelay(delayTime);
 
 			// move target letters down
-			GameObject[] tar = GameObject.FindGameObjectsWithTag (Constants.Tags.TAG_TARGET_LETTER);
+			GameObject[] tar = GameObject.FindGameObjectsWithTag(Constants.Tags.TAG_TARGET_LETTER);
 			foreach (GameObject letter in tar)
-				LeanTween.moveY (letter,-3f,time).setDelay(delayTime);
+				LeanTween.moveY(letter, -3f, time).setDelay(delayTime);
 
 			// play sound 
-			Debug.Log ("Playing clip for congrats");
-			AudioSource audio = go.AddComponent<AudioSource> ();
+			Debug.Log("Playing clip for congrats");
+			AudioSource audio = go.AddComponent<AudioSource>();
 			audio.clip = Resources.Load ("Audio/CongratsSound") as AudioClip;
-			audio.PlayDelayed (delayTime);
+			audio.PlayDelayed(delayTime);
 
 		}
+
 		void Update ()
 		{
 			// if user presses escape or 'back' button on android, exit program
-			if (Input.GetKeyDown (KeyCode.Escape))
-				Application.Quit ();
-		}	
+			if (Input.GetKeyDown(KeyCode.Escape))
+				Application.Quit();
+		}
 
 
 	}
